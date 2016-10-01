7e3254f1201d50e7e959d087ee8e73e71b79a87b
Balázs Szögi <szogibalu@gmail.com>
2016-10-01 20:23:28 +0200

    [MS-0] Separated unit and integration testing (#8)

--- src/main/java/com/microservicesteam/nutaxi/route/RouteService.java
+++ src/main/java/com/microservicesteam/nutaxi/route/RouteService.java
@@ -3,14 +3,12 @@ package com.microservicesteam.nutaxi.route;
 import static java.util.Arrays.stream;
 import static java.util.stream.Collectors.toList;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-import com.google.maps.model.DirectionsResult;
 import com.microservicesteam.nutaxi.route.googlemaps.GoogleMapsRouteService;
 
 @Service
@@ -25,22 +23,13 @@ public class RouteService {
 
     @Cacheable("routes")
     public Optional<Route> getRoute(RouteRequest request) {
-        Optional<DirectionsResult> directions = googleMapsRouteService.getDirections(request);
-
-        if (directions.isPresent()) {
-            return mapDirectionsToRoute(directions.get());
-        } else {
-            return Optional.empty();
-        }
-    }
-
-    private static Optional<Route> mapDirectionsToRoute(DirectionsResult directionsResult) {
-        List<String> overviewPolylines = stream(directionsResult.routes)
-                .map(googleMapsRoute -> googleMapsRoute.overviewPolyline.getEncodedPath()).collect(toList());
-
-        return Optional.of(Route.builder()
-                .overviewPolylines(overviewPolylines)
-                .build());
+        return googleMapsRouteService.getDirections(request).map(directions -> {
+            return Route.builder()
+                    .overviewPolylines(stream(directions.routes)
+                            .map(googleMapsRoute -> googleMapsRoute.overviewPolyline.getEncodedPath())
+                            .collect(toList()))
+                    .build();
+        });
     }
 
 }
